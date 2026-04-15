4f6cb6b2d1bc1e081d94d2330577ccef574b1427
Miguel Dovale <mikedovale@pm.me>
2026-04-15 09:27:39 -0700

    Slates: track generated parser, align Package.swift with swift-tree-sitter

--- Package.swift
+++ Package.swift
@@ -1,48 +1,72 @@
 // swift-tools-version:5.3
+
 import PackageDescription
 
 let package = Package(
     name: "TreeSitterPerl",
-    platforms: [.macOS(.v10_13), .iOS(.v11)],
     products: [
         .library(name: "TreeSitterPerl", targets: ["TreeSitterPerl"]),
     ],
-    dependencies: [],
-    targets: [
-        .target(name: "TreeSitterPerl",
-                path: ".",
-                exclude: [
-                    "Cargo.toml",
-                    "Makefile",
-                    "binding.gyp",
-                    "bindings/c",
-                    "bindings/go",
-                    "bindings/node",
-                    "bindings/python",
-                    "bindings/rust",
-                    "prebuilds",
-                    "grammar.js",
-                    "package.json",
-                    "package-lock.json",
-                    "pyproject.toml",
-                    "setup.py",
-                    "test",
-                    "examples",
-                    ".editorconfig",
-                    ".github",
-                    ".gitignore",
-                    ".gitattributes",
-                    ".gitmodules",
-                ],
-                sources: [
-                    "src/parser.c",
-                    "src/scanner.c"
-                ],
-                resources: [
-                    .copy("queries")
-                ],
-                publicHeadersPath: "bindings/swift",
-                cSettings: [.headerSearchPath("src")])
+    dependencies: [
+        .package(name: "SwiftTreeSitter", url: "https://github.com/tree-sitter/swift-tree-sitter", from: "0.9.0"),
     ],
-    cLanguageStandard: .c11
+    targets: [
+        .target(
+            name: "TreeSitterPerl",
+            dependencies: [],
+            path: ".",
+            exclude: [
+                "binding.gyp",
+                "bindings/c",
+                "bindings/go",
+                "bindings/node",
+                "bindings/python",
+                "bindings/rust",
+                "Cargo.toml",
+                "CMakeLists.txt",
+                "cpanfile",
+                "copy-to-release",
+                "go.mod",
+                "grammar.js",
+                "generate_keywords.pl",
+                "unicode_ranges.pl",
+                "lib",
+                "LICENSE",
+                "Makefile",
+                "package-lock.json",
+                "package.json",
+                "pyproject.toml",
+                "README.md",
+                "script",
+                "setup.py",
+                "src/grammar.json",
+                "src/node-types.json",
+                "test",
+                "tree-sitter-perl.pc",
+                "tree-sitter.json",
+                ".clang-format",
+                ".editorconfig",
+                ".eslintrc.cjs",
+                ".github",
+            ],
+            sources: [
+                "src/parser.c",
+                "src/scanner.c",
+            ],
+            resources: [
+                .copy("queries"),
+            ],
+            publicHeadersPath: "bindings/swift",
+            cSettings: [.headerSearchPath("src")],
+        ),
+        .testTarget(
+            name: "TreeSitterPerlTests",
+            dependencies: [
+                "SwiftTreeSitter",
+                "TreeSitterPerl",
+            ],
+            path: "bindings/swift/TreeSitterPerlTests",
+        ),
+    ],
+    cLanguageStandard: .c11,
 )
